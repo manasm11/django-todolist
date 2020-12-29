88d72de898204b14824ff5e6a1455e65d9b782f8
MANAS MISHRA <f20170546@pilani.bits-pilani.ac.in>
2020-12-30 02:41:18 +0530

    api working !!!

---
+++ todo_drf/api/urls.py
@@ -0,0 +1,16 @@
+from django.urls import re_path, path
+from .views import (
+    api_info_view,
+    task_list_view,
+    task_detail_view,
+    task_create_view,
+    task_update_view,
+    )
+
+urlpatterns = [
+    path('task-list/', task_list_view),
+    path('task-create/', task_create_view),
+    path('task-detail/<str:pk>/', task_detail_view),
+    path('task-update/<str:pk>/', task_update_view),
+    path('', api_info_view),
+]
---
+++ todo_drf/api/views.py
@@ -0,0 +1,63 @@
+from django.shortcuts import render
+from django.http import JsonResponse
+
+from rest_framework import status
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+
+from .models import TaskModel
+from .serializers import TaskSerializer
+# Create your views here.
+
+@api_view(['GET', 'POST'])
+def api_info_view(request, *args, **kwargs):
+    api_urls = {
+        "List  ":"/task-list/",
+        "Detail":"/task-detail/<str:pk>",
+        "Create":"/task-create/",
+        "Update":"/task-update/<str:pk>",
+        "Delete":"/task-delete/<str:pk>",
+    }
+    return Response(api_urls)
+
+@api_view(['GET'])
+def task_list_view(request, *args, **kwargs):
+    tasks = TaskModel.objects.all()
+    serializer = TaskSerializer(instance=tasks, many=True)
+    return Response(serializer.data, status=200)
+
+@api_view(['GET'])
+def task_detail_view(request, pk, *args, **kwargs):
+    task_exists = TaskModel.objects.filter(id=pk).exists()
+    if not task_exists: return Response({'message':'Task not found'}, status=404)
+    task = TaskModel.objects.get(id=pk)
+    serializer = TaskSerializer(instance=task, many=False)
+    return Response(serializer.data, status=200)
+
+@api_view(['POST'])
+def task_create_view(request, *args, **kwargs):
+    serializer = TaskSerializer(data=request.data, many=False)
+    if not serializer.is_valid(): return Response({'message':'Invalid request'}, status=400)
+    serializer.save()
+    return Response(serializer.data, status=200)
+
+@api_view(['POST'])
+def task_update_view(request, pk, *args, **kwargs):
+    task_exists = TaskModel.objects.filter(id=pk).exists()
+    if not task_exists: return Response({'message':'Task not found'}, status=404)
+    task = TaskModel.objects.get(id=pk)
+    serializer = TaskSerializer(instance=task, data=request.data, many=False)
+    if not serializer.is_valid(): return Response({'message':'Invalid request'}, status=400)
+    serializer.save()
+    return Response(serializer.data, status=200)
+    
+
+@api_view(['POST', 'DELETE'])
+def task_delete_view(request, pk, *args, **kwargs):
+    task_exists = TaskModel.objects.filter(id=pk).exists()
+    if not task_exists: return Response({'message':'Task not found'}, status=404)
+    task = TaskModel.objects.get(id=pk)
+    task.delete()
+    return Response({'message':'Task deleted'}, status=200)
+
+

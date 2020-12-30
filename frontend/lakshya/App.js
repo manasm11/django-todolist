e6f4afa0514e00c0dba5f16e8d6ed694804a3b4e
Lakshya <51817477+Headon1729@users.noreply.github.com>
2020-12-30 17:11:25 +0530

    Add files via upload

---
+++ frontend/lakshya/App.js
@@ -0,0 +1,12 @@
+import { TaskList } from './components';
+// import { useState, useEffect } from 'react';
+function App() {
+  return (
+    <>
+      <h3 id="heading">Welcome to ToDo List</h3>
+      <TaskList />
+    </>
+  );
+}
+
+export default App;

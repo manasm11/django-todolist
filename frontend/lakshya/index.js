f2e6ecf4bc8f4c4e43682bd817c5ec3c28c56947
MANAS MISHRA <f20170546@pilani.bits-pilani.ac.in>
2020-12-30 19:23:01 +0530

    gui is cool and working

--- frontend/lakshya/index.js
+++
@@ -1,4 +0,0 @@
-import SearchBar from './SearchBar';
-import TaskList from './TaskList';
-
-export {SearchBar, TaskList}
--- frontend/src/components/TaskList.js
+++
@@ -1,9 +0,0 @@
-import _TaskElement from './_TaskElement';
-
-export default function TaskList({tasks}){
-    return (
-        <div>
-            {tasks.map((task) => <_TaskElement key={task.id} task={task}/>)}
-        </div>
-    )
-}
